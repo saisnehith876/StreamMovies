0e025c3a39441566a07877c5e5de08f4acaa261c
saisnehith876 <saisnehith876@gmail.com>
2026-05-27 18:18:51 +0530

    Set up basic Gin server with hello endpoint

--- Server/main.go
+++ Server/main.go
@@ -1,1 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/hello", func(c *gin.Context) {
+		c.String(200, "hello,Movie_Streaming")
+	})
+
+	if err := router.Run(":8080"); err != nil {
+		fmt.Println("Failed to start server", err)
+	}
+
+}
 
