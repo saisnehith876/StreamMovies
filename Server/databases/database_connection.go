b781168d00cce8138ce61f2fa95d9e6abb5b4774
saisnehith876 <saisnehith876@gmail.com>
2026-05-27 18:39:28 +0530

    Create database_connection.go for MongoDB setup
    Added initial setup for database connection with MongoDB.

--- Server/databases/database_connection.go
+++ Server/databases/database_connection.go
@@ -1,1 +1,15 @@
+package database
 
+import(
+	"fmt"
+	"log"
+	"os" //reading env variables ->.env
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+
+)
+
+func DBInsatnce() *mongo.Client{
+	
+}
