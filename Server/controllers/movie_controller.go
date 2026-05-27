c8a4f5c9832ae451a83de1fccd9ee17fd0eb52d2
saisnehith876 <saisnehith876@gmail.com>
2026-05-27 18:20:07 +0530

    Implement GetMovies handler in movie_controller
    Add GetMovies function to return a list of movies

--- Server/controllers/movie_controller.go
+++ Server/controllers/movie_controller.go
@@ -1,1 +1,11 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetMovies() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "list of movies"})
+
+	}
+}
 
