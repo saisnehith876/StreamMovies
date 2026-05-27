f26bd8a4f84276c7fab1e936038716ec7dd84903
saisnehith876 <saisnehith876@gmail.com>
2026-05-27 18:24:31 +0530

    Add movies endpoint to main router

--- Server/main.go
+++ Server/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	controller "github.com/saisnehith876/StreamMovies/Server/StreamMovie_Server/controllers"
 )
 
 func main() {
@@ -13,9 +14,10 @@ func main() {
 		c.String(200, "hello,Movie_Streaming")
 	})
 
+	router.GET("/movies", controller.GetMovies())
+
 	if err := router.Run(":8080"); err != nil {
 		fmt.Println("Failed to start server", err)
 	}
 
 }
-
