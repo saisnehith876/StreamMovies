3e04386a6cb97732ac05012db634420fa9a05a50
Snehith876 <saisnehith876@gmail.com>
2026-05-29 00:22:09 +0530

    Implement movie fetching functionality

--- Server/controllers/movie_controller.go
+++ Server/controllers/movie_controller.go
@@ -1,11 +1,66 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/saisnehith876/StreamMovies/Server/StreamMovie_Server/database"
+	"github.com/saisnehith876/StreamMovies/Server/StreamMovie_Server/models"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+// var movieCollection *mongo.Collection = database.OpenCollection("movies")
+var movieCollection *mongo.Collection = database.OpenCollection("Movies", database.Client) //be careful of names, Movies is the databse name used
+
+//var validate = validator.New()
 
 func GetMovies() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "list of movies"})
+		ctx, cancel := context.WithTimeout(c, 100*time.Second)
+		defer cancel()
 
+		var movies []models.Movie
+		cursor, err := movieCollection.Find(ctx, bson.M{})
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch movies."})
+		}
+		defer cursor.Close(ctx)
+
+		if err = cursor.All(ctx, &movies); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode movies."})
+			return
+
+		}
+		c.JSON(http.StatusOK, movies)
 	}
 }
 
+func GetMovie() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		movieID := c.Param("imdb_id")
+		if movieID == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Movie ID is required"})
+			return
+		}
+
+		var movie models.Movie
+
+		err := movieCollection.FindOne(ctx, bson.M{"imdb_id": movieID}).Decode(&movie)
+
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Movie not found"})
+			return
+		}
+
+		c.JSON(http.StatusOK, movie)
+
+	}
+
+}
