15327835f13cf6d4be107a1cc94c16d6c59d1fc0
Snehith876 <saisnehith876@gmail.com>
2026-05-29 00:23:15 +0530

    Add route to get movie by IMDb ID

--- Server/main.go
+++ Server/main.go
@@ -15,6 +15,7 @@ func main() {
 	})
 
 	router.GET("/movies", controller.GetMovies())
+	router.GET("/movie/:imdb_id", controller.GetMovie())
 
 	if err := router.Run(":8080"); err != nil {
 		fmt.Println("Failed to start server", err)
