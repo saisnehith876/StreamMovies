331b23d42fbfd47a6ec2c73c47a68355e669bd0b
Snehith876 <saisnehith876@gmail.com>
2026-05-31 07:28:12 +0530

    Create user model struct for MongoDB
    Define user model struct for MongoDB with fields for user information and JWT.

--- Server/models/user_model.go
+++ Server/models/user_model.go
@@ -1,1 +1,26 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+type user struct {
+	ID        bson.ObjectID
+	UserId    string
+	FirstName string
+	LastNmae  string
+	Email     string
+	Password  string // need to hash it
+	Role      string //only 2 roles- admin or user
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Token     string
+
+	//we r going to use JWT/ Json web Tokens for authentication and  authorization purposes in this application
+	//we'll persist the values with revlant tokens within the documents tat is represented in code by this user struct
+	// a JWT is comoact URL safe token used to securely transmit info between parties as JSON object
+	//header -Payload -Signature 3 parts of JWT
+}
 
