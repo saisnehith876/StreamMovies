27ab364b629784e0c79605d05e3ee25d9c606df4
Snehith876 <saisnehith876@gmail.com>
2026-05-31 09:18:13 +0530

    Refactor user controller to include MongoDB integration

--- Server/controllers/user_controller.go
+++ Server/controllers/user_controller.go
@@ -1,4 +1,3 @@
-
 package controllers
 
 import (
@@ -8,11 +7,14 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
-	"golang.org/x/crypto/bcrypt"
-
-	//"github.com/saisnehith876/StreamMovies/Server/StreamMovie_Server/database"
+	"github.com/saisnehith876/StreamMovies/Server/StreamMovie_Server/database"
 	"github.com/saisnehith876/StreamMovies/Server/StreamMovie_Server/models"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
+
+var userCollection *mongo.Collection = database.OpenCollection("Users", database.Client)
 
 func HashPassword(password string) (string, error) {
 	HashPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
@@ -36,9 +38,37 @@ func RegisterUser() gin.HandlerFunc {
 			return
 		}
 		hashedPassword, err := HashPassword(user.Password)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Unable to hash password"})
+			return
+		}
 
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 		defer cancel()
+
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to check existiing user"})
+			return
+		}
+		if count > 0 {
+			c.JSON(http.StatusConflict, gin.H{"error": "User already. exists"})
+			return
+		}
+		user.UserID = bson.NewObjectID().Hex()
+		user.CreatedAt = time.Now()
+		user.UpdatedAt = time.Now()
+		user.Password = hashedPassword
+
+		result, err := userCollection.InsertOne(ctx, user)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
+			return
+		}
+
+		c.JSON(http.StatusCreated, result)
 
 	}
 
