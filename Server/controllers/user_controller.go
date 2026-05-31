9aa22a2bfd890c6b08698fd493a3507d1e2364ec
Snehith876 <saisnehith876@gmail.com>
2026-05-31 08:05:31 +0530

    Add user registration handler with password hashing
    Implement user registration with password hashing and validation.

--- Server/controllers/user_controller.go
+++ Server/controllers/user_controller.go
@@ -1,1 +1,45 @@
 
+package controllers
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"golang.org/x/crypto/bcrypt"
+
+	//"github.com/saisnehith876/StreamMovies/Server/StreamMovie_Server/database"
+	"github.com/saisnehith876/StreamMovies/Server/StreamMovie_Server/models"
+)
+
+func HashPassword(password string) (string, error) {
+	HashPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(HashPassword), nil
+}
+
+func RegisterUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var user models.User
+
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid input data"})
+		}
+		validate := validator.New()
+
+		if err := validate.Struct(user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Validation failed", "details": err.Error()})
+			return
+		}
+		hashedPassword, err := HashPassword(user.Password)
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+	}
+
+}
