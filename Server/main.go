cd6de215ef38f888ea6bb35d22b9f6a5be2a8f6e
Snehith876 <saisnehith876@gmail.com>
2026-06-15 01:49:48 +0530

    Refactor routes in main.go
    Commented out movie-related routes and added route setup functions.

--- Server/main.go
+++ Server/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
-	controller "github.com/saisnehith876/StreamMovies/Server/StreamMovie_Server/controllers"
+	"github.com/saisnehith876/StreamMovies/Server/StreamMovie_Server/routes"
 )
 
 func main() {
@@ -14,10 +14,14 @@ func main() {
 		c.String(200, "hello,Movie_Streaming")
 	})
 
-	router.GET("/movies", controller.GetMovies())
+	/*router.GET("/movies", controller.GetMovies())
 	router.GET("/movie/:imdb_id", controller.GetMovie())
 	router.POST("/addmovie", controller.AddMovie())
 	router.POST("/register", controller.RegisterUser())
+	router.POST("/login", controller.LoginUser()) */
+
+	routes.SetupUnProtectedRoutes(router)
+	routes.SetupProtectedRoutes(router)
 
 	if err := router.Run(":8080"); err != nil {
 		fmt.Println("Failed to start server", err)
