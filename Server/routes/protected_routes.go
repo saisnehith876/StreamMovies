dc6fde7e2dbbb83042407fc00afb4848cb791283
Snehith876 <saisnehith876@gmail.com>
2026-06-15 01:53:38 +0530

    Add protected routes for movie retrieval and addition

--- Server/routes/protected_routes.go
+++ Server/routes/protected_routes.go
@@ -1,1 +1,15 @@
 
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	controller "github.com/saisnehith876/StreamMovies/Server/StreamMovie_Server/controllers"
+	"github.com/saisnehith876/StreamMovies/Server/StreamMovie_Server/middleware"
+)
+
+func SetupProtectedRoutes(router *gin.Engine) {
+	router.Use(middleware.AuthMiddleWare())
+
+	router.GET("/movie/:imdb_id", controller.GetMovie())
+	router.POST("/addmovie", controller.AddMovie())
+}
